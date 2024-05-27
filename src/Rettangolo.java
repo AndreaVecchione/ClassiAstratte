9904d6be370c1a92e3a3a7c4099ebee1ba4ed4a0
AndreaVecchione <164344505+AndreaVecchione@users.noreply.github.com>
2024-05-27 19:02:14 +0200

    Rettangolo.java

--- src/Rettangolo.java
+++ src/Rettangolo.java
@@ -1,26 +1,10 @@
 public class Rettangolo extends formaAstratta {
-    private double altezza;
+    private final double altezza;
 
-    private double base;
-
-    public double getAltezza() {
-        return altezza;
-    }
+    private final double base;
 
     public Rettangolo(double altezza, double base) {
         this.altezza = altezza;
-        this.base = base;
-    }
-
-    public void setAltezza(double altezza) {
-        this.altezza = altezza;
-    }
-
-    public double getBase() {
-        return base;
-    }
-
-    public void setBase(double base) {
         this.base = base;
     }
 
